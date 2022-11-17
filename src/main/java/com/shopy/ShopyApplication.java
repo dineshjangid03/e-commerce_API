c574ca6f2886385c32d250be102b216029c80a82
Dinesh Kumar <dinesh.jangid323@gmail.com>
2022-11-17 18:04:49 +0530

    init

---
+++ src/main/java/com/shopy/ShopyApplication.java
@@ -0,0 +1,13 @@
+package com.shopy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopyApplication.class, args);
+	}
+
+}
