971d0549a7c6efbc6e9649d8e95f5e88354d62ab
Dinesh Kumar <dinesh.jangid323@gmail.com>
2022-11-23 11:34:20 +0530

    method for order between two date added

--- src/main/java/com/shopy/repository/BillRepo.java
+++ src/main/java/com/shopy/repository/BillRepo.java
@@ -1,11 +1,19 @@
 package com.shopy.repository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.shopy.model.Bill;
 
 @Repository
 public interface BillRepo extends JpaRepository<Bill, Integer>{
+	
+	@Query(value = "from Bill b where billDate BETWEEN :startDate AND :endDate")
+	public List<Bill> billBetweenDate(@Param("startDate")LocalDate startDate,@Param("endDate")LocalDate endDate);
 
 }
--- src/main/java/com/shopy/repository/OrderRepo.java
+++ src/main/java/com/shopy/repository/OrderRepo.java
@@ -1,11 +1,19 @@
 package com.shopy.repository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.shopy.model.Order;
 
 @Repository
 public interface OrderRepo extends JpaRepository<Order, Integer>{
+	
+	@Query(value = "from Order o where orderDate BETWEEN :startDate AND :endDate")
+	public List<Order> orderBetweenDate(@Param("startDate")LocalDate startDate,@Param("endDate")LocalDate endDate);
 
 }
